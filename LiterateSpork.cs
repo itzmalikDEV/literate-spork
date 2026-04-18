19e61ac9a2e191c56839142a1a6cf63b82a79d77
Malik <landorjan238@gmail.com>
2026-04-18 11:22:14 -0500

    Literate

---
+++ LiterateSpork.cs
@@ -0,0 +1,12 @@
+namespace LiterateSpork
+{
+    class LiterateSpork 
+    {
+        Console.WriteLine("");
+        Console.WriteLine("|| DEAD ||");
+
+        Console.WriteLine("Bedrock Sharp");
+        Console.WriteLine("BedorckSharp Console");
+        Console.WriteLine("Made by: XRookieFight and ItzMalikDev");
+    }
+}
